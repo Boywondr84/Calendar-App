aca559daf8df3e6686504db91b11b767d69a23a2
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-06-29 03:07:09 -0500

    added id # to each save button, started putting together save to localstorage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,19 +9,25 @@ displayDate();
 var saveButtons = document.querySelectorAll(".saveBtn");
 saveButtons.forEach(button => {
     button.addEventListener("click", function handleClick(event) {
-        console.log("button clicked", event);
+        console.log("button clicked", event.target);
+        // button.setAttribute("style", "background-color: pink");
     });
 });
+// added button id #'s to all saveBtns. Will this be useful when saving and retrieving?
 
+// When save button clicked, information from row into local storage(time & text) if/else statement??
+var saveButtons = {
+    time: "Hour",
+    text: "Meeting",
+}
+window.localStorage.setItem("schedule", JSON.stringify(saveButtons));
 
+// window.localStorage.clear();
 
 // Change text-area background color according to past, present, future
 // Text area needs a data index and time index
 
 // getHourEvent();
-
-// Save button - ultimately save info to local storage
-// $(".saveBtn").on("click", saveHourEvent)
 
 
 // var saveHourEvent = function() {
